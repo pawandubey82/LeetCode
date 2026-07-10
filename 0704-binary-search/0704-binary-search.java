b67710bf3408984b00cb96dd345674c050172552
pawan dubey <115526057+pawandubey82@users.noreply.github.com>
2026-07-10 15:34:45 +0530

    Time: 0 ms (100%), Space: 48.7 MB (14.03%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(target > arr[mid]){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+            mid = (start+end)/2;
+        }
+        return -1;
+    }
+}

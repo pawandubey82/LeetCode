e3f669a06ad45f39075ee5176bc8fcb252223f9d
pawan dubey <115526057+pawandubey82@users.noreply.github.com>
2026-07-16 07:31:58 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,27 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // xor with all array elemet
-        int xorSum =0;
-        for(int n:nums){
-            xorSum = xorSum ^ n;
-        }
-        // Xor with all element in range
         int n = nums.length;
-        for(int i=0;i<=n ;i++){
-            xorSum = xorSum ^i;
+        int ans = 0;
+        for(int i = 1; i <= n;i++){
+            ans = ans ^ i;
         }
-        //ans mil jaega
-        return xorSum;
+        for(int i = 0; i< n;i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+
+
+        // // xor with all array elemet
+        // int xorSum =0;
+        // for(int n:nums){
+        //     xorSum = xorSum ^ n;
+        // }
+        // // Xor with all element in range
+        // int n = nums.length;
+        // for(int i=0;i<=n ;i++){
+        //     xorSum = xorSum ^i;
+        // }
+        // //ans mil jaega
+        // return xorSum;
     }
 }
